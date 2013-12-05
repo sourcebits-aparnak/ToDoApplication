89ea1142aad4473168a89bfeb17ece484106b008
sourcebits-aparnak <aparna.k@sourcebits.com>
2013-12-05 16:33:22 +0530

    FragmentTest class included.

---
+++ ToDoDemo/test/com/sb/tododemo/FragmentTest.java
@@ -0,0 +1,10 @@
+package com.sb.tododemo;
+
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class FragmentTest {
+
+}
