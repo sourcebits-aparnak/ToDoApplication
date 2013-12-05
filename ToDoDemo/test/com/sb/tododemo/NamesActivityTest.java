9d5b7a8a28eb245434ba914c8bba693afe16fb0f
sourcebits-nagaraj <nagaraj.nayak@sourcebits.com>
2013-12-05 16:27:21 +0530

    included adapter test cases

---
+++ ToDoDemo/test/com/sb/tododemo/NamesActivityTest.java
@@ -0,0 +1,32 @@
+
+
+import android.app.Activity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.sb.tododemo.NamesActivity;
+import com.sb.tododemo.R;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class NamesActivityTest {
+    @Test
+    public void shouldShowAListOfNames() throws Exception {
+        Activity activity = Robolectric.buildActivity(NamesActivity.class).create().get();
+
+        ListView namesListView = (ListView) activity.findViewById(R.id.names_list);
+
+        // ListViews only create as many children as will fit in their bounds, so make it big...
+        namesListView.layout(0, 0, 100, 1000);
+
+        TextView nameRow = (TextView) namesListView.getChildAt(1);
+        assertThat(nameRow.getText().toString(), equalTo("Donald Rumsfeld"));
+    }
+}
---
+++ ToDoDemo/test/com/sb/tododemo/NamesAdapterTest.java
@@ -0,0 +1,69 @@
+
+
+import android.app.Activity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.sb.tododemo.NamesAdapter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class NamesAdapterTest {
+
+    private ArrayList<String> names;
+    private NamesAdapter namesAdapter;
+
+    @Before
+    public void setUp() throws Exception {
+        names = new ArrayList<String>();
+        names.add("Lauren Conrad");
+        names.add("Heidi Montag");
+        names.add("Kim Kardashian");
+        namesAdapter = new NamesAdapter(names);
+    }
+
+    @Test
+    public void testGetCount() throws Exception {
+        assertThat(namesAdapter.getCount(), equalTo(names.size()));
+    }
+
+    @Test
+    public void testGetItem() throws Exception {
+        assertThat((String) namesAdapter.getItem(0), equalTo("Lauren Conrad"));
+        assertThat((String) namesAdapter.getItem(1), equalTo("Heidi Montag"));
+        assertThat((String) namesAdapter.getItem(2), equalTo("Kim Kardashian"));
+    }
+
+    @Test
+    public void testGetItemId() throws Exception {
+        assertThat(namesAdapter.getItemId(0), equalTo(0L));
+        assertThat(namesAdapter.getItemId(1), equalTo(1L));
+        assertThat(namesAdapter.getItemId(2), equalTo(2L));
+    }
+
+    @Test
+    public void testGetView() throws Exception {
+        Activity activity = Robolectric.buildActivity(Activity.class).create().get();
+        TextView nameView = (TextView) namesAdapter.getView(1, null, new LinearLayout(activity));
+        assertThat(nameView.getText().toString(), equalTo("Heidi Montag"));
+    }
+
+    @Test
+    public void shouldRecycleProvidedViews() throws Exception {
+        Activity activity = Robolectric.buildActivity(Activity.class).create().get();
+        TextView existingView = new TextView(Robolectric.application);
+        TextView nameView = (TextView) namesAdapter.getView(2, existingView, new LinearLayout(activity));
+        assertThat(nameView, sameInstance(existingView));
+    }
+}
