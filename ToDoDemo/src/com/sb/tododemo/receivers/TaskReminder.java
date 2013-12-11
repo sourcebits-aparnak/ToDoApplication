fa05f58f545b01221653b1df28e535adf6491815
sourcebits-aparnak <aparna.k@sourcebits.com>
2013-12-11 10:48:29 +0530

    Minor changes included.

--- ToDoDemo/src/com/sb/tododemo/receivers/TaskReminder.java
+++ ToDoDemo/src/com/sb/tododemo/receivers/TaskReminder.java
@@ -9,25 +9,27 @@ import android.widget.Toast;
 import com.sb.tododemo.databases.MyTodoContentProvider;
 import com.sb.tododemo.databases.TodoTable;
 
-public class TaskReminder extends BroadcastReceiver{
+public class TaskReminder extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        
+
         String id = intent.getExtras().getString("ID");
-        Cursor cursor = context.getContentResolver().query(MyTodoContentProvider.CONTENT_URI, null, TodoTable.COLUMN_ID + "=?",
-                new String[] { id }, null);
-        
-        if(cursor != null && cursor.getCount() > 0) {
-            cursor.moveToFirst();   
+        Cursor cursor = context.getContentResolver().query(MyTodoContentProvider.CONTENT_URI, null, TodoTable.COLUMN_ID + "=?", new String[] { id },
+                null);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            cursor.moveToFirst();
             final String category = cursor.getString(cursor.getColumnIndex(TodoTable.COLUMN_CATEGORY));
             final String description = cursor.getString(cursor.getColumnIndex(TodoTable.COLUMN_DESCRIPTION));
             final String summary = cursor.getString(cursor.getColumnIndex(TodoTable.COLUMN_SUMMARY));
-            Toast.makeText(context, category + "\n" + description + "\n" + summary , Toast.LENGTH_LONG).show();
+            Toast.makeText(context, category + "\n" + description + "\n" + summary, Toast.LENGTH_LONG).show();
         }
-        
-        //Toast.makeText(context, "Alarm triggered!!!", Toast.LENGTH_LONG).show();
-        
+
+        if (cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+
     }
 
 }
